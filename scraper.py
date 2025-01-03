e9c3c43da6b70824f13462078dbbfefc5a49b03a
BillyTaj <billyc59@gmail.com>
2025-01-03 17:16:01 -0500

    WIP: messing with html and json
    We need a neat way to read the HTML to grab the datetime.

--- scraper.py
+++ scraper.py
@@ -2,15 +2,34 @@ import os
 import sys
 import urllib
 import urllib.request
-
+import json
+import xmltojson
+#https://www.geeksforgeeks.org/convert-html-source-code-to-json-object-using-python/
 #----------------------------------------------
 #webscraper to automate DB updates from various sources.
 #https://realpython.com/python-web-scraping-practical-introduction/
 if __name__ == "__main__":
     place_to_pull = "https://huttenhower.sph.harvard.edu/humann2_data/chocophlan/"
-    page_obj = urllib.request.urlopen(place_to_pull)
-     
+    ncbi = "https://ftp.ncbi.nlm.nih.gov/blast/db/"
+    
+    #page_obj = urllib.request.urlopen(ncbi)
+    page_obj = urllib.request.urlopen(place_to_pull) 
+   
     page_bytes = page_obj.read()
     html = page_bytes.decode("utf-8")
 
+    line = html.split("\t")
     
+    print(line)
+    for item in line:
+        count = 0
+        if(".tar.gz" in item):
+            cleaned_line = item.strip("\n")
+            #cleaned_line = cleaned_line.strip(" ")
+            #cleaned_line = cleaned_line.strip("\t")
+            #cleaned_line = cleaned_line.split("</a>")
+            print(count, cleaned_line)
+            count += 1
+            #for i in cleaned_line:
+            #    print(count, i)
+            #    count += 1
